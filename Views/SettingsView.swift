5ab04755b474ebcff60db3803b806db4e01bf3e3
Timothy Lewis <129003345+NamefulEntity@users.noreply.github.com>
2023-10-18 20:05:28 -0400

    Swift files containing changes

---
+++ Views/SettingsView.swift
@@ -0,0 +1,146 @@
+//
+//  SettingsView.swift
+//  Basic-Car-Maintenance
+//
+//  Created by Mikaela Caron on 8/19/23.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+
+	@StateObject private var viewModel: SettingsViewModel
+	@State private var isShowingAddVehicle = false
+	@State private var showDeleteVehicleError = false
+	@State var errorDetails: Error?
+	@ObservedObject var authenticationViewModel: AuthenticationViewModel
+
+	init(authenticationViewModel: AuthenticationViewModel) {
+		self._viewModel = StateObject(wrappedValue: SettingsViewModel(authenticationViewModel: authenticationViewModel)) // swiftlint:disable:this line_length
+		self.authenticationViewModel = authenticationViewModel
+	}
+
+	var body: some View {
+		NavigationStack {
+			Form {
+				Text("Thanks for using this app! It's open source and anyone can contribute to it.")
+
+				Link(destination: URL(string: "https://github.com/mikaelacaron/Basic-Car-Maintenance")!) {
+					Label {
+						Text("GitHub Repo")
+					} icon: {
+						Image("github-logo")
+							.resizable()
+							.frame(width: 20, height: 20)
+					}
+				}
+
+				// swiftlint:disable:next line_length
+				Link(destination: URL(string: "https://github.com/mikaelacaron/Basic-Car-Maintenance/issues/new?assignees=&labels=feature+request&projects=&template=feature-request.md&title=FEATURE+-")!) {
+					Label {
+						Text("Request a New Feature")
+					} icon: {
+						Image(systemName: "doc.badge.plus")
+							.resizable()
+							.frame(width: 20, height: 20)
+					}
+				}
+
+				// swiftlint:disable:next line_length
+				Link(destination: URL(string: "https://github.com/mikaelacaron/Basic-Car-Maintenance/issues/new?assignees=&labels=bug&projects=&template=bug-report.md&title=BUG+-")!) {
+					Label {
+						Text("Report a Bug")
+					} icon: {
+						Image(systemName: "ladybug")
+							.resizable()
+							.frame(width: 20, height: 20)
+					}
+				}
+
+				NavigationLink {
+					ContributorsListView(viewModel: viewModel)
+				} label: {
+					HStack {
+						Image(systemName: "person.3.fill")
+						Text("Contributors")
+					}
+				}
+
+				Section {
+					ForEach(viewModel.vehicles) { vehicle in
+						VStack {
+							Text(vehicle.name)
+								.font(.headline)
+
+							Text(vehicle.make)
+
+							Text(vehicle.model)
+						}
+						.swipeActions {
+							Button("Delete", role: .destructive) {
+								Task {
+									do {
+										try await viewModel.deleteVehicle(vehicle)
+									} catch {
+										errorDetails = error
+										showDeleteVehicleError = true
+									}
+								}
+							}
+						}
+					}
+					.swipeActions(edge: .trailing) {
+						Button {
+							isShowingAddVehicle.toggle()
+						} label: {
+							Text("Add Vehicle")
+						}
+					} header: {
+						Text("Vehicles")
+					}
+					.updateVehicle(vehicleName: String) {
+				}
+					Section {
+						NavigationLink {
+							AuthenticationView(viewModel: authenticationViewModel)
+						} label: {
+							Label {
+								Text("Profile")
+							} icon: {
+								Image(systemName: "person")
+							}
+						}
+					}
+
+					Text("Version \(Bundle.main.versionNumber) (\(Bundle.main.buildNumber))")
+				}
+				.alert("Failed To Delete Vehicle", isPresented: $showDeleteVehicleError) {
+					Button("OK") {
+						showDeleteVehicleError = false
+					}
+				} message: {
+					if let errorDetails {
+						Text("Failed To Delete Vehicle\nDetails:\(errorDetails.localizedDescription)")
+					} else {
+						Text("Failed To Add Vehicle. Unknown Error.")
+					}
+				}
+				.navigationTitle(Text("Settings"))
+				.task {
+					await viewModel.getVehicles()
+				}
+				.sheet(isPresented: $isShowingAddVehicle) {
+					AddVehicleView() { vehicle in
+						Task {
+							await viewModel.addVehicle(vehicle)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	#Preview {
+		SettingsView(authenticationViewModel: AuthenticationViewModel())
+	}
+}
